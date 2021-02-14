3e5e985a9b3f2e69f526c277d1b2c65cef979e8e
1canewton <1canewton@seattleschools.org>
2021-02-13 19:20:26 -0800

    Update Collapsible List

--- src/components/CollapsableSingle.js
+++ src/components/CollapsableSingle.js
@@ -1,41 +1,50 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { Component, useContext, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { Context as WorkoutContext } from "../context/WorkoutContext";
 import Collapsible from "react-native-collapsible";
 
-export default class CollapsibleSingle extends Component {
-  state = {
-    collapsed: true,
+const CollapsableSingle = ({ title, children, navigation }) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleExpanded = () => {
+    setCollapsed(!collapsed);
   };
 
-  toggleExpanded = () => {
-    this.setState({ collapsed: !this.state.collapsed });
-  };
-
-  render() {
-    const { header, headerText, workout, workout2, workoutList, time } = styles;
-    const { title, children } = this.props;
-
-    return (
-      <View>
-        <TouchableOpacity onPress={this.toggleExpanded}>
-          <View style={header}>
-            <Text style={headerText}>{title}</Text>
-          </View>
-        </TouchableOpacity>
-        <Collapsible collapsed={this.state.collapsed} align="center">
-          <View style={workoutList}>
-            <View style={workout}>
-              <Text style={time}>{children}</Text>
-            </View>
-            <View style={workout}>
-              <Text style={time}>{children}</Text>
-            </View>
-          </View>
-        </Collapsible>
-      </View>
-    );
-  }
-}
+  return (
+    <View>
+      <TouchableOpacity onPress={toggleExpanded}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+      <Collapsible collapsed={collapsed} align="center">
+        <View style={styles.workoutList}>
+          <FlatList
+            data={children}
+            keyExtractor={(index) => index.id}
+            scrollEnabled={false}
+            renderItem={({ item }) => {
+              return (
+                <TouchableOpacity>
+                  <View style={styles.workout}>
+                    <Text style={styles.time}>{item.data}</Text>
+                    <Text style={styles.date}>{item.date}</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+      </Collapsible>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   header: {
@@ -64,6 +73,9 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   workout2: {
     paddingVertical: 25,
@@ -86,4 +98,10 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     color: "white",
   },
+  date: {
+    fontSize: 12,
+    color: "white",
+  },
 });
+
+export default CollapsableSingle;
--- src/screens/WorkoutListScreen.js
+++ src/screens/WorkoutListScreen.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import { Context as WorkoutContext } from "../context/WorkoutContext";
 import CollapsibleSingle from "../components/CollapsableSingle";
+import { ScrollView } from "react-native-gesture-handler";
 
 const WorkoutListScreen = ({ navigation }) => {
   const { state, addWorkout } = useContext(WorkoutContext);
@@ -16,67 +17,16 @@ const WorkoutListScreen = ({ navigation }) => {
 
   return (
     <View>
-      <CollapsibleSingle title="100 meters" children="00:10.43" />
-      <CollapsibleSingle title="200 meters" children="00:21.68" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <CollapsibleSingle title="100 meters" children={state} />
+        {/* <CollapsibleSingle title="200 meters" children="00:21.68" />
       <CollapsibleSingle title="400 meters" children="00:28.32" />
-      <CollapsibleSingle title="800 meters" children="00:51.66" />
-      <FlatList
-        data={state}
-        keyExtractor={(workout) => workout.id}
-        renderItem={({ item }) => {
-          // return (
-          //   <TouchableOpacity>
-          //     <View style={styles.workout}>
-          //       <Text style={styles.time}>{item.data}</Text>
-          //       <Text style={styles.date}>{item.date}</Text>
-          //     </View>
-          //   </TouchableOpacity>
-          // )
-        }}
-      />
+      <CollapsibleSingle title="800 meters" children="00:51.66" /> */}
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  workout: {
-    marginLeft: 5,
-    marginRight: 5,
-    marginTop: 5,
-    borderRadius: 5,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 15,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "gray",
-    alignItems: "center",
-    backgroundColor: "#f38375",
-  },
-  date: {
-    fontSize: 12,
-  },
-  time: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  distance: {
-    fontSize: 18,
-    color: "black",
-    fontWeight: "bold",
-  },
-  distanceView: {
-    paddingVertical: 8,
-    backgroundColor: "#EF6351",
-    alignItems: "center",
-  },
-  wrapperCollapsibleList: {
-    flex: 1,
-    marginTop: 20,
-    overflow: "hidden",
-    backgroundColor: "#FFF",
-    borderRadius: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default WorkoutListScreen;
