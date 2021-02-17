e3cdbfe96a6508e0e72b1339e4a45fd6c3c6d268
1canewton <1canewton@seattleschools.org>
2021-02-17 12:58:03 -0800

    Cleaning Up

--- src/screens/WorkoutDetailScreen.js
+++ src/screens/WorkoutDetailScreen.js
@@ -7,8 +7,6 @@ const WorkoutDetailScreen = ({ route }) => {
     <View>
       <View style={styles.header}>
         <Text style={styles.title}>Distance</Text>
-        <Text style={styles.text}>Luisa was here :) </Text>
-
       </View>
       <Text style={styles.text}>Distance: {distance}</Text>
       <Text style={styles.text}>Time: {data}</Text>
