cbd96450b32273534e4a89486e039a46af0e7f1c
charlie b <45837976+cbennington852@users.noreply.github.com>
2021-02-17 19:20:33 -0800

    basic graph test and demo

--- src/screens/WorkoutDetailScreen.js
+++ src/screens/WorkoutDetailScreen.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
 
 const WorkoutDetailScreen = ({ route }) => {
   const { distance, data, date } = route.params;
+
+  const datadumb = {
+    labels: [0,0,0,0,0,0,0,0,0,0,0,0],
+    datasets: [
+      {
+        data: [49.31,49.36,49.38,49.85,50.36,50.86,51.35,51.86,52.4,52.87,53.34,53.8],
+        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+        strokeWidth: 2 // optional
+      }
+    ],
+    legend: ["running data(dummy)"] // optional
+  };
+
   return (
     <View>
       <View style={styles.header}>
@@ -12,11 +33,33 @@ const WorkoutDetailScreen = ({ route }) => {
       <Text style={styles.text}>Time: {data}</Text>
       <View style={styles.header}>
         <Text style={styles.title}>Velocity</Text>
+
+      </View>
+      <View>
+      <LineChart
+          data={datadumb}
+          width={300}
+          height={256}
+          verticalLabelRotation={30}
+          chartConfig={chartConfig}
+          bezier
+        />
       </View>
       <Text style={styles.text}>Top Speed:</Text>
       <Text style={styles.text}>Top Speed Timestamp:</Text>
     </View>
   );
+};
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
 };
 
 const styles = StyleSheet.create({
