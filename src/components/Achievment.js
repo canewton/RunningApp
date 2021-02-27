ed6549a22866f1b649e56c27fe170bd58773d8f0
Luisa Bloch Garcia <blochgarcialuisa@Luisas-MacBook-Pro.local>
2021-02-26 21:36:55 -0800

    very scuffed achievment stuff

---
+++ src/components/Achievment.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+
+const Acheivement = (props) => {
+    return <View>
+        <Text>{props.title}</Text>
+        <FlatList
+            keyExtractor={(goals) => goals.title}
+            data = {goals}
+            renderItem={({ item }) => {
+            <Text>
+                {item.title}: {item.requirements}
+            </Text>
+            }}
+        />
+    </View>
+}
+
+const StyleSheet=style.create({})
+
+
+export default Acheivement;
--- src/screens/AchievementsScreen.js
+++ src/screens/AchievementsScreen.js
@@ -1,15 +1,33 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Context as WorkoutContext } from "../context/WorkoutContext";
+import Acheivement from "../components/Achievment";
 
 const AchievementsScreen = () => {
   //green color: #8fd14f
   //blue color: #12cdd4
   const { state } = useContext(WorkoutContext);
 
+  // can we put an array in this bitch?
+
   return (
     <View>
-      <Text>Challenges Screen</Text>
+      <Acheivement
+        title="Run 100 meters"
+        // const goals = 
+        //   {name: "gold", requirements: },
+        //   {name: "silver", requirements: },
+        //   {name: "bronze", requirements: },
+        // ]
+      />
+      <Acheivement
+        title="Run 4 seasons"
+      //   const goals = [
+      //     {name: "gold", requirements: },
+      //     {name: "silver", requirements: },
+      //     {name: "bronze", requirements: },
+      //   ]
+       />
     </View>
   );
 };
@@ -17,3 +35,30 @@ const AchievementsScreen = () => {
 const styles = StyleSheet.create({});
 
 export default AchievementsScreen;
+
+/*
+overall qs:
+will we need to be able to click on them to see more info?? 
+our list and key extractor + their location is very scuffed
+what do we display on the opening page? how?
+where do we compare the data to the requirments (i assume here?)
++LUCA DESIGN??
+
+hello
+
+
+still todo
+progress bar
+all styling
+comparing data
+individual functions
+
+
+done
+will format text of the passed in requirment 
+
+list possible acheivemnts and their requirments in  achivements screen
+that will pass in info on individual acheivements and 
+their medal requirments - component will format it and screen will display it
+
+*/
