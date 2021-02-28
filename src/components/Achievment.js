1be55a39a7a195dda96061769023155d7a2bb61a
1canewton <1canewton@seattleschools.org>
2021-02-28 14:57:58 -0800

    fix hooks error

--- src/components/Achievment.js
+++ src/components/Achievment.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,23 +8,21 @@ import {
 } from "react-native";
 import Collapsible from "react-native-collapsible";
 
+const Acheivement = ({ title }) => {
+  //create a state variable that defines if the collapsible list is collapsed or not
+  const [collapsed, setCollapsed] = useState(false);
 
-//create a state variable that defines if the collapsible list is collapsed or not
-const [collapsed, setCollapsed] = useState(false);
-
-//a function that collapses or expands the collapsible list
-const toggleExpanded = () => {
-  setCollapsed(!collapsed);
-};
-
-
-const Acheivement = ({title}) => {
-    return <View>
-        <TouchableOpacity onPress={toggleExpanded}> 
-            <View style={styles.title}>
-                <Text style={styles.Text}>{title}</Text>
-            </View>
-        </TouchableOpacity>
+  //a function that collapses or expands the collapsible list
+  const toggleExpanded = () => {
+    setCollapsed(!collapsed);
+  };
+  return (
+    <View>
+      <TouchableOpacity onPress={toggleExpanded}>
+        <View style={styles.title}>
+          <Text style={styles.Text}>{title}</Text>
+        </View>
+      </TouchableOpacity>
 
       {/*Make a list that collapses and expands. Make the list start out as expanded.*/}
       <Collapsible collapsed={collapsed} align="center">
@@ -37,37 +35,34 @@ const Acheivement = ({title}) => {
             keyExtractor={(goals) => goals.medal}
             scrollEnabled={false}
             renderItem={({ item }) => {
-                return (
-                    <Text>
-                      {" "}
-                      {item.medal}
-                    </Text>
-                );
+              return <Text> {item.medal}</Text>;
             }}
-            />
+          />
         </View>
       </Collapsible>
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-   title: {
-      marginLeft: 5,
-      marginRight: 5,
-      marginTop: 5,
-      borderRadius: 5,
-      paddingVertical: 18,
-      paddingHorizontal: 10,
-      justifyContent: "space-between",
-      borderWidth: 1,
-      borderColor: "gray",
-      backgroundColor: "#8fd14f",
-    },
-    Text: {
-        textAlign: "left",
-        fontSize: 30,
-        color: "white",
-        fontWeight: "500",}
-})
+  title: {
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
+    borderRadius: 5,
+    paddingVertical: 18,
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: "gray",
+    backgroundColor: "#8fd14f",
+  },
+  Text: {
+    textAlign: "left",
+    fontSize: 30,
+    color: "white",
+    fontWeight: "500",
+  },
+});
 
 export default Acheivement;
