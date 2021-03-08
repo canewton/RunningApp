c38a1c152d6bb0dc43b1dee5221a8153a1598b33
1canewton <1canewton@seattleschools.org>
2021-03-07 17:32:28 -0800

    Created Connect Button with custom icon

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { createIconSetFromFontello } from "react-native-vector-icons";
-//import fontelloConfig from "../config.json";
-//import fontelloConfig from "../resources/fonts/fontelloConfig/config.json";
-import { FontAwesome } from "@expo/vector-icons";
-
-//const Icon = createIconSetFromFontello(fontelloConfig);
+import ConnectIcon from "../icons/ConnectIcon";
 
 const ConnectButton = () => {
   return (
     <View style={styles.margin}>
       <View style={styles.circle}>
-        <FontAwesome name="bluetooth-b" color="#8fd14f" size={70} />
-        <Text style={styles.text}>Connect</Text>
+        <ConnectIcon />
       </View>
     </View>
   );
