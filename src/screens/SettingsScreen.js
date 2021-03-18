8a144a5391ffc5d7e5ba3cc4bb13f61cf428fcd7
orlandoluce <80439051+orlandoluce@users.noreply.github.com>
2021-03-17 19:05:57 -0700

    ahhhh

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -8,12 +8,52 @@ const SettingsScreen = () => {
   const { state } = useContext(WorkoutContext);
 
   return (
+    <View style={styles.headerStyle}>
+      <Text style={styles.title}>Profile</Text>
+    </View>,
+
     <View>
-      <Text>Settings Screen</Text>
+      <Image source={require('../../assets/icon.png')}/>
+      <Text style={styles.text}>First Last</Text>
+      <Text style={styles.text}>  - 2 friends</Text>
+      <Text style={styles.text}>  - more profile info</Text>
+
+    </View>,
+
+    <View style={styles.headerStyle}>
+      <Text style={styles.title}>Settings</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  headerStyle:{
+    backgroundColor: "#008000",
+    paddingVertical: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 25,
+    justifyContent: "center",
+  },
+  header: {
+      backgroundColor: "#12cdd4",
+      paddingVertical: 10,
+      flexDirection: "row",
+      alignItems: "center",
+      marginVertical: 25,
+      justifyContent: "center",
+  },
+  title: {
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 20,
+   },
+  text: {
+      fontSize: 18,
+      marginLeft: 10,
+  },
+});
 
 export default SettingsScreen;
+
