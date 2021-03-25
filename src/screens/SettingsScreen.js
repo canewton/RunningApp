e89edc291a91f6633c3ee74183bb86d208619e90
1canewton <1canewton@seattleschools.org>
2021-03-24 20:55:35 -0700

    Orlando Screen Fixed

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,17 +1,18 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { Context as WorkoutContext } from "../context/WorkoutContext";
 
-profileImage='../../assets/icon.png'
-friendNumber='#'
-rankNumber='#'
-profileName='First Last'
-hundredMeterTime= '00:00:00'
-twoHundredMeterTime= '00:00:00'
-threeHundredMeterTime= '00:00:00'
-twoHundredMeterDay='mm/dd/yy'
-hundredMeterDay='mm/dd/yy'
-threeHundredMeterDay='mm/dd/yy'
+const profileImage = "../../assets/icon.png";
+const teamName = "team";
+const friendNumber = "#";
+const rankNumber = "#";
+const profileName = "First Last";
+const hundredMeterTime = "00:00:00";
+const twoHundredMeterTime = "00:00:00";
+const threeHundredMeterTime = "00:00:00";
+const twoHundredMeterDay = "mm/dd/yy";
+const hundredMeterDay = "mm/dd/yy";
+const threeHundredMeterDay = "mm/dd/yy";
 
 const SettingsScreen = () => {
   //green color: #8fd14f
@@ -19,46 +20,53 @@ const SettingsScreen = () => {
   const { state } = useContext(WorkoutContext);
 
   return (
-    <View style={styles.headerStyle}>
-      <Text style={styles.title}>Profile</Text>
-    </View>,
+    <View>
+      <View style={styles.headerStyle}>
+        <Text style={styles.title}>Profile</Text>
+      </View>
 
-    <View >
-      <Image source={require(profileImage)}/>
+      <View>
+        {/* <Image source={require(profileImage)}/> */}
+        <TouchableOpacity>
+          <View style={styles.text}>
+            <Text>Change Photo</Text>
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.textBig}>{profileName}</Text>
+        <TouchableOpacity>
+          <View style={styles.text}>
+            <Text>Edit Profile</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
 
-      <TouchableOpacity >
-        <View style={styles.text}>
-      <Change Photo />
-        </View>
-      </TouchableOpacity>,
+      <View style={styles.blueStyle}>
+        <Text style={styles.title}> Team: {teamName} </Text>
+        <Text style={styles.title}> - {friendNumber} Friends</Text>
+        <Text style={styles.title}> - Rank {rankNumber}</Text>
+      </View>
 
-      <Text style={styles.textBig}>{profileName}</Text>
+      <View style={styles.headerStyle}>
+        <Text style={styles.title}>Top Runs</Text>
+      </View>
 
-      <TouchableOpacity >
-        <View style={styles.text}>
-      <Edit Profile />
-        </View>
-      </TouchableOpacity>,
+      <View style={styles.blueStyle}>
+        <Text style={styles.smallTitle}>
+          {" "}
+          100 meter: {hundredMeterTime} {hundredMeterDay}{" "}
+        </Text>
 
-      
-    </View>,
+        <Text style={styles.smallTitle}>
+          {" "}
+          200 meter: {twoHundredMeterTime} {twoHundredMeterDay}
+        </Text>
 
-    <View style={styles.blueStyle}>
-       <Text style={styles.title}>  Team: {teamName} </Text>,
-       <Text style={styles.title}>  - {friendNumber} Friends</Text>,
-       <Text style={styles.title}>  - Rank {rankNumber}</Text>,
-    </View> ,
-
-    <View style={styles.headerStyle}>
-      <Text style={styles.title}>Top Runs</Text>
-    </View>,
-
-    <View style={styles.blueStyle}>
-       <Text style={styles.smallTitle}>  100 meter: {hundredMeterTime} {hundredMeterDay} </Text>,
-       <Text style={styles.smallTitle}>  200 meter: {twoHundredMeterTime} {twoHundredMeterDay}</Text>,
-       <Text style={styles.smallTitle}>  300 meter: {threeHundredMeterTime} {threeHundredMeterDay}</Text>,
-    </View> 
-
+        <Text style={styles.smallTitle}>
+          {" "}
+          300 meter: {threeHundredMeterTime} {threeHundredMeterDay}
+        </Text>
+      </View>
+    </View>
   );
 };
 
@@ -72,12 +80,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   blueStyle: {
-      backgroundColor: "#12cdd4",
-      paddingVertical: 10,
-      flexDirection: "row",
-      alignItems: "center",
-      marginVertical: 25,
-      justifyContent: "center",
+    backgroundColor: "#12cdd4",
+    paddingVertical: 10,
+    alignItems: "center",
+    marginVertical: 25,
+    justifyContent: "center",
   },
   title: {
     color: "white",
@@ -85,8 +92,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   text: {
-      fontSize: 14,
-      marginLeft: 10,
+    fontSize: 14,
+    marginLeft: 10,
   },
   textBig: {
     fontSize: 20,
