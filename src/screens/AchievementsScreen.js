18f6bcad54b4be907336ebcdb5626a227b867907
Odessa gray <odessargray@gmail.com>
2021-04-02 23:04:16 -0700

    idk what this is

--- src/screens/AchievementsScreen.js
+++ src/screens/AchievementsScreen.js
@@ -120,6 +120,8 @@ const AchievementsScreen = () => {
   );
 };
 
+const workouts = Object.values(state)[0];
+
 const styles = StyleSheet.create({
   icons: {
     flexDirection: "row",
@@ -156,30 +158,3 @@ const styles = StyleSheet.create({
 
 export default AchievementsScreen;
 
-/*
-overall qs:
-will we need to be able to click on them to see more info?? 
-our list and key extractor + their location is very scuffed
-what do we display on the opening page? how?
-where do we compare the data to the requirments (i assume here?)
-+LUCA DESIGN??
-
-
-
-still todo
-progress bar
-requirments styling
-comparing data
-individual functions
-
-ERROR:
-collapsable goals??
-
-done
-will format text of the passed in requirment 
-
-list possible acheivemnts and their requirments in  achivements screen
-that will pass in info on individual acheivements and 
-their medal requirments - component will format it and screen will display it
-
-*/
