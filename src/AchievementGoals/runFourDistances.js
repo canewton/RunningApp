258bb5dbb5e62a6dcda15260c00f9c9898d0cc22
Odessa gray <odessargray@gmail.com>
2021-04-03 14:05:10 -0700

    fixed run 4 distances

--- src/AchievementGoals/runFourDistances.js
+++ src/AchievementGoals/runFourDistances.js
@@ -1,40 +1,35 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 
-const done = false;
-const runonehundred = false
-const runtwohundred = false
-const runfourhundred = false
-const runeighthundred = false
+var done = false;
+var runonehundred = false;
+var runtwohundred = false;
+var runfourhundred = false;
+var runeighthundred = false;
 
-const runFourDistances = ({ workouts }) => {
+const RunFourDistances = ({ workouts }) => {
   for (var i = 0; i < workouts.length; i++) {
     if (workouts[i].getdistance === "400 meters") {
       runfourhundred = true;
     }
     if (workouts[i].getdistance === "200 meters") {
         runtwohundred = true;
-      }
+    }
     if (workouts[i].getdistance === "100 meters") {
         runonehundred = true;
-      }
-      if (workouts[i].getdistance === "800 meters") {
+    }
+    if (workouts[i].getdistance === "800 meters") {
         runeighthundred = true;
-      } 
+    } 
     
     if (runonehundred && runtwohundred && runfourhundred && runeighthundred) {
       done = true;
     }
   }
-
-    return (
-    <View>
-        <Text>  </Text>
-    </View>
-    )
+  console.log (done)
 };
 
 const styles = StyleSheet.create({});
 
 
-export default DaysInAWeekGoals;
+export default RunFourDistances;
