bf8c279ccb7be93bf9eb334be870bd9911aec425
1canewton <1canewton@seattleschools.org>
2021-04-03 18:40:04 -0700

    Add stuff

--- src/components/TopRunsFilter.js
+++ src/components/TopRunsFilter.js
@@ -1,85 +1,34 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import WorkoutList from "./WorkoutList";
 
-const TopRunsFilter = (state) => {
- const workouts = Object.values(state)[0];
- const filterWorkoutsByRank = (Rank) => {
-    return workouts.filter((workout) => {
-      return (
-        workout.Rank===(1)
-      );
+const TopRunsFilter = ({ state }) => {
+  const filterWorkoutsByRank = (rank) => {
+    return state.filter((workout) => {
+      return workout.rank === rank;
     });
   };
+  console.log(filterWorkoutsByRank(1));
   return (
-      <View>
-        <WorkoutList
-         title="100 meters"
-         workouts={filterWorkoutsByRank(
-         "100 meters",
-         workoutsSortedByDate[dateIndex]
-         )}
-        />
-        <WorkoutList
-         title="200 meters"
-         workouts={filterWorkoutsRank(
-         "200 meters",
-         workoutsSortedByDate[dateIndex]
-         )}
-        />
-        <WorkoutList
-         title="400 meters"
-         workouts={filterWorkoutsByRank(
-         "400 meters",
-         workoutsSortedByDate[dateIndex]
-         )}
-        />
-        <WorkoutList
-         title="800 meters"
-         workouts={filterWorkoutsByRank(
-         "800 meters",
-         workoutsSortedByDate[dateIndex]
-         )}
-        />
-        <WorkoutList
-         title="1600 meters"
-         workouts={filterWorkoutsByRank(
-         "1600 meters",
-         workoutsSortedByDate[dateIndex]
-         )}
-         />
+    <View>
+      <FlatList
+        data={filterWorkoutsByRank(1)}
+        keyExtractor={(index) => index.id}
+        renderItem={({ item }) => {
+          return <Text>{item.distance}</Text>;
+        }}
+      />
     </View>
-  )
-
+  );
 };
 
-const styles = StyleSheet.create({
-  text: {
-    fontWeight: "400",
-    fontSize: 15,
-  },
-  container: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 10,
-    marginBottom: 10,
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  iconLeft: {
-    fontSize: 24,
-    color: "black",
-  },
-  iconRight: {
-    fontSize: 24,
-    color: "black",
-    transform: [{ rotateY: "180deg" }],
-  },
-  iconButton: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default TopRunsFilter;
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,9 +1,18 @@
 import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  TextInput,
+} from "react-native";
 import { Context as WorkoutContext } from "../context/WorkoutContext";
 import { Ionicons } from "@expo/vector-icons";
-import EditProfileScreen from "../screens/EditProfileScreen"
+import EditProfileScreen from "../screens/EditProfileScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import TopRunsFilter from "../components/TopRunsFilter";
 
 const profileImage = "../../assets/icon.png";
 const teamName = "Ballard";
@@ -15,25 +24,24 @@ const threeHundredMeterTime = "00:00:00";
 const twoHundredMeterDay = "mm/dd/yy";
 const hundredMeterDay = "mm/dd/yy";
 const threeHundredMeterDay = "mm/dd/yy";
-const totalDistance = "3240"
-const totalTime = "1h 20min 23"
+const totalDistance = "3240";
+const totalTime = "1h 20min 23";
 
 //const navigator=createStackNavigator(
-  //{
+//{
 
 //  }
 //)
+const windowWidth = Dimensions.get("window").width;
 
-const SettingsScreen = props => {
+const SettingsScreen = (props) => {
   //green color: #8fd14f
   //blue color: #12cdd4
   const { state } = useContext(WorkoutContext);
-  const [profileName, setProfileName] = useState('First Last')
-  const [teamName, setTeamName] = useState('Ballard')
-  //const onPressEdit 
-   //
-  
-
+  const [profileName, setProfileName] = useState("First Last");
+  const [teamName, setTeamName] = useState("Ballard");
+  //const onPressEdit
+  //
 
   return (
     <View>
@@ -55,15 +63,14 @@ const SettingsScreen = props => {
         </TouchableOpacity>
         <Text style={styles.textBig}>{profileName}</Text>
         <TouchableOpacity
-          //onPress={() => navigation.navigate("EditProfile") }
+        //onPress={() => navigation.navigate("EditProfile") }
         >
           <View>
             <Text style={styles.text}>Edit Profile</Text>
           </View>
         </TouchableOpacity>
       </View>
-      
-    
+
       {/*<TextInput 
         style={styles.input}
         autoCapitalize="none"
@@ -77,13 +84,13 @@ const SettingsScreen = props => {
         <Text style={styles.title}>Rank: {rankNumber}</Text>
       </View>
 
-      
       <View style={styles.row}>
         <View style={styles.greyStyle}>
           <Text style={styles.title}>Top Runs</Text>
-          <Text style={styles.smallTitle}>
-           {" "}
-           100 meter: {hundredMeterTime} {hundredMeterDay}{" "}
+          <TopRunsFilter state={state} />
+          {/* <Text style={styles.smallTitle}>
+            {" "}
+            100 meter: {hundredMeterTime} {hundredMeterDay}{" "}
           </Text>
 
           <Text style={styles.smallTitle}>
@@ -94,31 +101,19 @@ const SettingsScreen = props => {
           <Text style={styles.smallTitle}>
             {" "}
             300 meter: {threeHundredMeterTime} {threeHundredMeterDay}
-          </Text>
+          </Text> */}
         </View>
         <View style={styles.Column}>
           <View style={styles.greyStyle}>
-            <Text style={styles.smallTitle}>
-              {" "}
-              TOTAL DISTANCE
-            </Text>
-            <Text style={styles.smallTitle}>
-              {" "}
-              {totalDistance} m
-            </Text>
+            <Text style={styles.smallTitle}> TOTAL DISTANCE</Text>
+            <Text style={styles.smallTitle}> {totalDistance} m</Text>
             <TouchableOpacity style={styles.blueStyle}>
               <Text style={styles.white}>Compare</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.greyStyle}>
-            <Text style={styles.smallTitle}>
-              {" "}
-              TOTAL TIME
-            </Text>
-            <Text style={styles.smallTitle}>
-              {" "}
-              {totalTime}sec
-            </Text>
+            <Text style={styles.smallTitle}> TOTAL TIME</Text>
+            <Text style={styles.smallTitle}> {totalTime}sec</Text>
             <TouchableOpacity style={styles.blueStyle}>
               <Text style={styles.white}>Compare</Text>
             </TouchableOpacity>
@@ -145,8 +140,9 @@ const styles = StyleSheet.create({
     marginVertical: 20,
     justifyContent: "center",
     borderRadius: 20,
-    borderColor:"#8fd14f",
-    borderWidth: 5
+    borderColor: "#8fd14f",
+    borderWidth: 5,
+    width: windowWidth / 2,
   },
   blueStyle: {
     backgroundColor: "#12cdd4",
@@ -156,7 +152,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     borderRadius: 20,
     borderWidth: 5,
-    borderColor: "#12cdd4"
+    borderColor: "#12cdd4",
   },
   textStyle: {
     paddingVertical: 10,
@@ -191,19 +187,19 @@ const styles = StyleSheet.create({
   },
   input: {
     margin: 15,
-    borderColor: 'black',
-    borderWidth: 1
+    borderColor: "black",
+    borderWidth: 1,
   },
   Column: {
-    Flexdirection: 'Column'
+    flexDirection: "column",
   },
   row: {
-    Flexdirection: 'row'
+    flexDirection: "row",
   },
   white: {
-    color: 'white',
-    fontWeight: "bold"
-  }
+    color: "white",
+    fontWeight: "bold",
+  },
 });
 
 export default SettingsScreen;
